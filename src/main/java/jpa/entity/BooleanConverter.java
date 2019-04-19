fe10dbe8af17643ec3f1621f766fa3b81a359ba0
swethacts <SwethaReddy.Baikadi@cognizant.com>
2019-04-19 14:12:35 +0530

    Add files via upload

---
+++ src/main/java/jpa/entity/BooleanConverter.java
@@ -0,0 +1,28 @@
+package jpa.entity;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter
+public class BooleanConverter implements AttributeConverter<Boolean, Integer> {
+
+	public Integer convertToDatabaseColumn(Boolean aBoolean) {
+		if (Boolean.TRUE.equals(aBoolean)) {
+			return 1;
+		} else {
+			return -1;
+		}
+	}
+
+	public Boolean convertToEntityAttribute(Integer value) {
+		if (value == null) {
+			return Boolean.FALSE;
+		} else {
+			if (value == 1) {
+				return Boolean.TRUE;
+			} else {
+				return Boolean.FALSE;
+			}
+		}
+	}
+}
